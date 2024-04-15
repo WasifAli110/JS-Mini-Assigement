c5ed4649561f9aa61501af92aa9c27f44587664f
WasifAli110 <154609808+WasifAli110@users.noreply.github.com>
2024-04-16 00:48:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let myArr = [[2, 3, 4, 4, 4, 4], [5, 5], [5], [9, 8, 7]];
+
+
+//console.log(myArr[0] [0]);
+//console.log(myArr[0] [1]);
+//console.log(myArr[0] [2]);
+//console.log(myArr[0] [3]);
+//console.log(myArr[0] [4]);
+//console.log(myArr[0] [5]);
+//console.log(myArr[1] [0]);
+//console.log(myArr[1] [1]);
+//console.log(myArr[2] [0]);
+//console.log(myArr[3] [0]);
+//console.log(myArr[3] [1]);
+//console.log(myArr[3] [2]);
+
+
+
+for (let i = 0; i < myArr.length; i++) {
+    for (let j = 0; j < myArr[i].length; j++) {
+        console.log(myArr[i][j]);
+    }
+}
