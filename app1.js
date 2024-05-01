d422c6a90b75e88ab4dcba9551c3dd4f010aad2b
WasifAli110 <154609808+WasifAli110@users.noreply.github.com>
2024-05-01 20:42:26 +0500

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,4 +1,4 @@
-let matrix1 = [
+/*let matrix1 = [
   [1, 3, 4],
   [0, 5, 5],
   [9, 6, 1],
@@ -26,4 +26,31 @@ for (let i = 0; i < 3; i++) {
   for (let j = 0; j < 3; j++) {
     console.log(matrix1[i][j] + matrix2[i][j]);
   }
+}*/
+
+let arr1 = [
+  [2,2,2],
+  [5,6,6],
+  [7,1,0],
+];
+
+let arr2 = [
+  [3,2,1],
+  [6,6,7],
+  [7,1,1],
+];
+
+
+/*
+[5,4,3],
+[11,12,13],
+[14,2,1],
+*/
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+      console.log(arr1[i][j]  +   arr2[i][j] );
+      
+  }
+  
 }
